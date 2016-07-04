8eb706553dc374c2673baf567f811b9f9253d29c
Your Name <you@example.com>
2016-07-04 21:01:33 -0230

    add cutoff

--- Gradiant/Main.py
+++ Gradiant/Main.py
@@ -20,83 +20,94 @@ print ("Test element is {}".format(EleName))
 print ("Basis set is {}".format(args.basis))
 print ("Level of theory is {}".format(args.theory))
 print ("The value of Delta is {}".format(args.delta))
-
+print ("The cutoff is {}".format(args.limit))
 
 sto=elementMod.GetSTO(Z,args.basis)
-## Guess file name  ##
-GuessFile='Guess_'+str(Z)+'_'+elementMod.GetElemSym(Z).strip()+'_'+args.basis.strip()+'.txt'
-GradFile='Grad_'+str(Z)+'_'+elementMod.GetElemSym(Z).strip()+'_'+args.basis.strip()+'.txt'
-EnergyFileI='EnergyI_'+str(Z)+'_'+elementMod.GetElemSym(Z).strip()+'_'+args.basis.strip()
-EnergyFileF='EnergyF_'+str(Z)+'_'+elementMod.GetElemSym(Z).strip()+'_'+args.basis.strip()
 
-### Read Guess scale values from the file ####
-if os.path.isfile(GuessFile):
-	guessScale=[]
-	file=open(GuessFile,'r')
-	for line in file:
-		guessScale.append(line.rstrip('\n'))
-	file.close()
-else:
-	guessScale=[str(args.initial)]*len(sto)
-	file=open(GuessFile,'w')
-	for index,sto_out in enumerate(sto):
-		file.write(str(args.initial)+'\n')
-	file.close()
+## files names  ##
+fileName=str(Z)+'_'+elementMod.GetElemSym(Z).strip()+'_'+args.basis.strip()
+GuessFile='Guess_'+fileName+'.txt'
+GradFile='Grad_'+fileName+'.txt'
+EnergyFileI='EnergyI_'+fileName
+EnergyFileF='EnergyF_'+fileName
 
-# calculate the energy
-OEnergy=elementMod.Get_Energy(EnergyFileI,Z,args.charge,args.theory,args.basis,guessScale)
+
 
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-### Generate Scale values to find Gradiant
-AlphaValue=[]
-for index,sto_out in enumerate(guessScale):
-    tempScale=guessScale[:]
-    tempScale[index]=str(float(tempScale[index])+args.delta)
-    AlphaValue.append(tempScale)
-for index,sto_out in enumerate(guessScale):
-    tempScale=guessScale[:]
-    tempScale[index]=str(float(tempScale[index])-args.delta)
-    AlphaValue.append(tempScale)
-### 
-#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-### Generate INPUT FILE ###
-job=[]
-title=EleName.strip()+'_'+args.basis.strip()
-for index,sto_out in enumerate(AlphaValue):
-    title2=title+'_scale_'+str(index+1)
-    job.append(title2)
-    file=open(title2+'.gfj','w')
-    file.write(elementMod.returnInput(Z,args.charge,args.theory,args.basis,sto_out)+'\n\n')
-    file.close()
+DeltaVal=args.delta
+DEnergy=999.99
+CurrCutOff=-args.limit
+while DEnergy > CurrCutOff:
+    ### Read Guess scale values from the file ####
+    if os.path.isfile(GuessFile):
+        guessScale=[]
+        file=open(GuessFile,'r')
+        for line in file:
+            guessScale.append(line.rstrip('\n'))
+        file.close()
+    else:
+        guessScale=[str(args.initial)]*len(sto)
+        file=open(GuessFile,'w')
+        for index,sto_out in enumerate(sto):
+            file.write(str(args.initial)+'\n')
+        file.close()
+     
+    # calculate the energy
+    OEnergy=elementMod.Get_Energy(EnergyFileI,Z,args.charge,args.theory,args.basis,guessScale)
 
-# run the bash file
-EnergyGrad=[]
-for ijob in job:
-    subprocess.call('g09 < '+ijob+'.gfj > '+ijob+'.out\n',shell=True)
-    Genergy=subprocess.check_output('grep "SCF Done:" '+ijob+'.out | tail -1|awk \'{ print $5 }\'', shell=True)
-    EnergyGrad.append(Genergy.decode('ascii').rstrip('\n'))
 
-# calculate Gradiant
-Grad=[]
-half_len_grid=int(len(EnergyGrad)/2)
-for val in range(half_len_grid):
-    Grad.append((float(EnergyGrad[val])-float(EnergyGrad[val+half_len_grid]))/2.0*args.delta)
-
-# calculate the new scale values
-guessScale=[float(i) - j for i, j in zip(guessScale, Grad)] 
-
-# calculate the new energy
-NEnergy=elementMod.Get_Energy(EnergyFileF,Z,args.charge,args.theory,args.basis,guessScale)
-DEnergy=NEnergy-OEnergy
-print(guessScale, NEnergy, OEnergy, DEnergy)
-
-# store the new scale values 
-if DEnergy < 0.0:
-	file=open(GuessFile,'w')
-	for val in guessScale:
-		file.write(str(val)+'\n')
-	file.close()
-else:
-	print('Dalta is large, \nIncrease in the energy\nExit')
-	sys.exit(0)
-#subprocess.call(['qsub',title+'.sh'])
+    ### Generate Scale values to find Gradiant
+    AlphaValue=[]
+    for index,sto_out in enumerate(guessScale):
+        tempScale=guessScale[:]
+        tempScale[index]=str(float(tempScale[index])+DeltaVal)
+        AlphaValue.append(tempScale)
+    for index,sto_out in enumerate(guessScale):
+        tempScale=guessScale[:]
+        tempScale[index]=str(float(tempScale[index])-DeltaVal)
+        AlphaValue.append(tempScale)
+    ### 
+    #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    ### Generate INPUT FILE ###
+    job=[]
+    title=EleName.strip()+'_'+args.basis.strip()
+    for index,sto_out in enumerate(AlphaValue):
+        title2=title+'_scale_'+str(index+1)
+        job.append(title2)
+        file=open(title2+'.gfj','w')
+        file.write(elementMod.returnInput(Z,args.charge,args.theory,args.basis,sto_out)+'\n\n')
+        file.close()
+    
+    # run the bash file
+    EnergyGrad=[]
+    for ijob in job:
+        subprocess.call('g09 < '+ijob+'.gfj > '+ijob+'.out\n',shell=True)
+        Genergy=subprocess.check_output('grep "SCF Done:" '+ijob+'.out | tail -1|awk \'{ print $5 }\'', shell=True)
+        EnergyGrad.append(Genergy.decode('ascii').rstrip('\n'))
+    
+    # calculate Gradiant
+    Grad=[]
+    half_len_grid=int(len(EnergyGrad)/2)
+    for val in range(half_len_grid):
+        Grad.append((float(EnergyGrad[val])-float(EnergyGrad[val+half_len_grid]))/2.0*DeltaVal)
+    
+    # calculate the new scale values
+    guessScale=[float(i) - j for i, j in zip(guessScale, Grad)] 
+    
+    # calculate the new energy
+    NEnergy=elementMod.Get_Energy(EnergyFileF,Z,args.charge,args.theory,args.basis,guessScale)
+    DEnergy=NEnergy-OEnergy
+    print(guessScale, NEnergy, OEnergy, DEnergy)
+    
+    # store the new scale values 
+    if DEnergy < 0.0:
+        file=open(GuessFile,'w')
+        for val in guessScale:
+            file.write(str(val)+'\n')
+        file.close()
+    else:
+        DeltaVal=DeltaVal*0.75
+        print('Change dalta value to '+ str(DeltaVal))
+        #sys.exit(0)
+    #subprocess.call(['qsub',title+'.sh'])
+    
