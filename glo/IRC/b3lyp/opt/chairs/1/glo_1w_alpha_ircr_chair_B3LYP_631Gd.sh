0a57254bb54f2193b86cd858024183a313e356c2
Your Name <you@example.com>
2017-01-15 16:02:21 -0600

    opt IRC

---
+++ glo/IRC/b3lyp/opt/chairs/1/glo_1w_alpha_ircr_chair_B3LYP_631Gd.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# Script for running gaussian
+
+#PBS -l walltime=23:00:00
+#PBS -l mem=2000mb
+#PBS -r n
+#PBS -l nodes=1:ppn=2
+
+cd $PBS_O_WORKDIR #makes it easy to return to the directory from which you submitted the job:
+module load gaussian
+g09 < /home/iawad/RayBasisSet/glo/IRC/b3lyp/opt/./chairs/1/glo_1w_alpha_ircr_chair_B3LYP_631Gd.com > /home/iawad/RayBasisSet/glo/IRC/b3lyp/opt/./chairs/1/glo_1w_alpha_ircr_chair_B3LYP_631Gd.out
